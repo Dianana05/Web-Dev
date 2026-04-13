88e480d5d1fdfb04c346ae9ac27f32b3425d0d83
unknown <diana.ryskulbekova07@mail.ru>
2026-04-13 13:09:51 +0500

    Upload lab10 into to lab8

--- Lab8/api/urls.py
+++ Lab8/api/urls.py
@@ -1,8 +1,17 @@
-from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet
+from django.urls import path
+from api.views import (
+    ProductListAPIView,
+    ProductDetailAPIView,
+    CategoryListAPIView,
+    CategoryDetailAPIView,
+    CategoryProductsAPIView
+)
 
-router = DefaultRouter()
-router.register(r'products', ProductViewSet, basename='products')
-router.register(r'categories', CategoryViewSet, basename='categories')
+urlpatterns = [
+    path('products/', ProductListAPIView.as_view()),
+    path('products/<int:product_id>/', ProductDetailAPIView.as_view()),
 
-urlpatterns = router.urls
+    path('categories/', CategoryListAPIView.as_view()),
+    path('categories/<int:category_id>/', CategoryDetailAPIView.as_view()),
+    path('categories/<int:category_id>/products/', CategoryProductsAPIView.as_view()),
+]
